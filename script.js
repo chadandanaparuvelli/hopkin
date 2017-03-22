8274a063d3574396aefbe5208d3ed4fc5ec77c8b
chadandanaparuvelli <paruvelli.chandana@gmail.com>
2017-03-21 18:31:42 -0700

    Update and rename style.css to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (i=0; i<names.length; i++)
+
+{
+		var firstLetter = names[i].charAt(0);
+		if (firstLetter === 'J')
+ 	{
+      speak1(names[i]);
+
+  	} else {
+
+       speak(names[i]);
+ 	}
+}
+
